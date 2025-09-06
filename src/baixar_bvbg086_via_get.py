a389652eba17fdf43e00e30fd9cea397289df6a4
Neuri <neuripereira@gmail.com>
2025-09-06 12:30:18 -0300

    alterado caminhos hardcode por variavel de ambiente com .env

--- src/baixar_bvbg086_via_get.py
+++ src/baixar_bvbg086_via_get.py
@@ -2,12 +2,14 @@ import os #manipulação de diretórios e caminhos de arquivos
 import requests #biblioteca HTTP usada para baixar arquivos
 from datetime import datetime #manipulação de datas
 
+from src.config import SRC_DOWNLOADS
+
 """Função para baixar arquivos
    Parâmetros:
         data_str:data em formato "YYYY-MM-DD" (ex: "2025-06-30"). Se não passar nada, pega a data de hoje.
         pasta_destino: onde salvar o arquivo (./downloads por padrão). 
 """
-def baixar_pregao_zip(data_str=None, pasta_destino="./downloads"):
+def baixar_pregao_zip(data_str=None, pasta_destino=SRC_DOWNLOADS):
     if data_str is None:
         data = datetime.today()
     else:
